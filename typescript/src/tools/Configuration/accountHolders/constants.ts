8476c7039080ebaa69ed439c2be83415f017ace3
anreyesj <148469061+anreyesj@users.noreply.github.com>
2025-07-10 15:22:16 +0000

    Add tool: get_account_holder

---
+++ typescript/src/tools/Configuration/accountHolders/constants.ts
@@ -0,0 +1,16 @@
+export const GET_ACCOUNT_HOLDER_NAME = "get_account_holder";
+export const GET_ACCOUNT_HOLDER_DESCRIPTION = `Returns an account holder's details and capabilities. 
+
+    Args:
+        id (str): The unique identifier for the account holder
+
+    Returns:
+        str: The raw response from the Adyen API containing the AccountHolder object. 
+
+    Notes:
+        - The 'id' parameter is the path parameter and is mandatory. If necessary, ask the user to provide this data.
+
+    Examples:
+        get_account_holder({id: "AHXXXXXXXXXXXXXXXXXXXXXXXXX"})
+        # Returns the raw response from the Adyen API, containing the AccountHolder object.`;
+
---
+++ typescript/src/tools/Configuration/accountHolders/index.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { Client, Types, BalancePlatformAPI } from "@adyen/api-library";
+import {
+  GET_ACCOUNT_HOLDER_NAME,
+  GET_ACCOUNT_HOLDER_DESCRIPTION
+} from "./constants";
+import { Tool } from "../../types";
+
+const getAccountHolderRequestShape: z.ZodRawShape = {
+  id: z.string()
+};
+
+const getAccountHolderObject = z.object(getAccountHolderRequestShape);
+
+const getAccountHolder = async (
+  client: Client,
+  getAccountHolderRequest: z.infer<typeof getAccountHolderObject> 
+) => {
+  const {id} = getAccountHolderRequest;
+  const balancePlatformApi = new BalancePlatformAPI(client)
+  try {
+     return await balancePlatformApi.AccountHoldersApi.getAccountHolder(id)
+  } catch (e) {
+    return "Failed to get account holder. Error: " + JSON.stringify(e);
+  }
+}
+
+export const getAccountHolderTool: Tool = {
+  name: GET_ACCOUNT_HOLDER_NAME,
+  description: GET_ACCOUNT_HOLDER_DESCRIPTION,
+  arguments: getAccountHolderObject,
+  invoke: getAccountHolder,
+};
