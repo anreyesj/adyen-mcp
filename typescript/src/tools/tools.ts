4487b4d2c55162d8dfa61fbaf0b9c083ba431e3f
anreyesj <148469061+anreyesj@users.noreply.github.com>
2025-08-27 16:26:30 +0000

    Register onboarding tools

--- typescript/src/tools/tools.ts
+++ typescript/src/tools/tools.ts
@@ -3,6 +3,9 @@ import { Tool } from "./types";
 import {cancelPaymentTool, refundPaymentTool} from "./modifications";
 import {createPaymentSessionTool, getPaymentMethodsTool, getPaymentSessionTool} from "./payments";
 import {getMerchantAccountsTool, listMerchantAccountsTool} from "./management";
+import {getAccountHolderTool} from "./Configuration/accountHolders"
+import { createHostedOnboardingLinkTool } from "./LegalEntityManagement/onboardingLinks";
+import { getLegalEntityTool } from "./LegalEntityManagement/legalEntities";
 
 export const tools: Tool[] = [
   createPaymentLinkTool,
@@ -14,5 +17,8 @@ export const tools: Tool[] = [
   getPaymentMethodsTool,
   listMerchantAccountsTool,
   getMerchantAccountsTool,
-  cancelPaymentTool
+  cancelPaymentTool,
+  getLegalEntityTool, 
+  getAccountHolderTool, 
+  createHostedOnboardingLinkTool
 ];
