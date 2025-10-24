fa974f9f2f3de7cc7a68c7bd96119935ae16203f
Andres <148469061+anreyesj@users.noreply.github.com>
2025-10-24 13:31:19 -0500

    Fix return type in account holder constants

--- typescript/src/tools/configuration/accountHolders/constants.ts
+++ typescript/src/tools/configuration/accountHolders/constants.ts
@@ -5,7 +5,7 @@ export const GET_ACCOUNT_HOLDER_DESCRIPTION = `Returns an account holder's detai
         id (str): The unique identifier for the account holder
 
     Returns:
-        str: The raw response from the Adyen API containing the AccountHolder object. 
+        object: The raw response from the Adyen API containing the AccountHolder object. 
 
     Notes:
         - The 'id' parameter is the path parameter and is mandatory. If necessary, ask the user to provide this data.
